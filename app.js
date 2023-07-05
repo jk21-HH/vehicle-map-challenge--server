3c7d9b9d1e8b80462804619a39f12dd0ea517315
jk21-HH <jeanya12@gmail.com>
2023-07-05 19:58:32 +0300

    fix error on failed get request

--- app.js
+++ app.js
@@ -5,22 +5,9 @@ const app = express();
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", function (req, res) {
-  res.sendFile(
-    path.join(__dirname, "../client/build/index.html"),
-    function (err) {
-      if (err) {
-        res.status(500).send(err);
-      }
-    }
-  );
-});
-
 const vehiclesRoute = require("./routes/Vehicles");
 app.use("/vehicles", vehiclesRoute); // reroute to vehicles
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, function () {
+app.listen(5000, function () {
   console.log("we are up and running");
 });
