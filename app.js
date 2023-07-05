a364465e0fca5a04d30915a93f56c612175c8375
jk21-HH <jeanya12@gmail.com>
2023-07-05 21:50:40 +0300

    fix port

--- app.js
+++ app.js
@@ -8,11 +8,13 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-const serverURL = process.env.serverURL || "http://localhost:5000";
+const serverURL = process.env.PORT || 5000;
+
+console.log(serverURL);
 
 const vehiclesRoute = require("./routes/Vehicles");
 app.use("/vehicles", vehiclesRoute); // reroute to vehicles
 
-app.listen(5000, function () {
+app.listen(serverURL, function () {
   console.log("we are up and running");
 });
