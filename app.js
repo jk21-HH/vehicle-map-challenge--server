4704aefcf38cec0d734f20d46c0d25e42676338a
jk21-HH <jeanya12@gmail.com>
2023-07-05 21:30:25 +0300

    maybe cors

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 const serverURL = process.env.serverURL || "http://localhost:5000";
 
