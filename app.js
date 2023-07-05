845226541cb46fc686931f6a2f9f2f12e49e89a4
jk21-HH <jeanya12@gmail.com>
2023-07-05 20:59:02 +0300

    fix the url cross origin

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+require("dotenv").config();
 
 const app = express();
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+const serverURL = process.env.serverURL || "http://localhost:5000";
 
 const vehiclesRoute = require("./routes/Vehicles");
 app.use("/vehicles", vehiclesRoute); // reroute to vehicles
